1d5e1fade3b7c8a02e0b8ba5e61f97065d6b5d22
Abdallah khattab <boodykhattab97@gmail.com>
2025-04-28 18:27:10 +0300

    -

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/ecommerce-api', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
